cfb0b3acc5033169a7c5d84bd2b4467f2f2e130d
Atul Acharya <atul.acharya@gmail.com>
2020-12-28 11:04:42 -0800

    upd: made add-markers and pick-object work together

--- P5-HomeServiceRobot/catkin_ws/src/scripts/pick_objects.sh
+++ P5-HomeServiceRobot/catkin_ws/src/scripts/pick_objects.sh
@@ -12,9 +12,9 @@ PATH_CATKIN_WS="/home/robond/workspace2/Robotics-ND/P5-HomeServiceRobot/catkin_w
 echo "Launching Turtlebot in World..."
 echo  "cd ${PATH_CATKIN_WS} && source devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${PATH_CATKIN_WS}/src/map/newworld.world " 
 echo " "
-xterm -title world -bg olive -e " cd ${PATH_CATKIN_WS} && source devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${PATH_CATKIN_WS}/src/map/newworld.world " & 
+xterm -geom 70x100 -bg olive -e " cd ${PATH_CATKIN_WS} && source devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${PATH_CATKIN_WS}/src/map/newworld.world " & 
 
-sleep 7
+sleep 10
 
 # 2 - View Navigation Launch
 # Open the workspace, source and launch view_navigation.launch
@@ -31,5 +31,13 @@ echo " cd ${PATH_CATKIN_WS} && source devel/setup.bash && roslaunch turtlebot_ga
 echo " "
 xterm -title AMCL -e " cd ${PATH_CATKIN_WS} && source devel/setup.bash && roslaunch turtlebot_gazebo amcl_demo.launch map_file:=${PATH_CATKIN_WS}/src/map/map.yaml " & 
 
+sleep 3
+
+xterm -e "  cd ${PATH_CATKIN_WS} && source devel/setup.bash && rostopic echo /move_base/goal " &
+
+sleep 8
+
 #4 - Pick Objects Launch
-#xterm -bg darkblue -geom 80x40 -e " roslaunch pick_objects pick_objects.launch " &
+echo "[launching pick objects]"
+echo  " roslaunch pick_objects pick_objects.launch " 
+xterm -bg darkblue -geom 80x40 -e " roslaunch pick_objects pick_objects.launch " &
